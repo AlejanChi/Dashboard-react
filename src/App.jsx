2e08cb46227790dc918e9ef1a93ab25acf87e69b
alejandro <veleiroalejandro@gmail.com>
2023-10-04 13:37:03 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Sidebar from "./components/Sidebar.jsx"
+
+function App(){
+  return <div>
+    <Sidebar/>
+    Hola mundo
+  </div>
+}
+
+export default App
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,7 @@
+function Sidebar(){
+    return <div className="bg-gray-800 ">
+        Sidebar
+    </div>
+}
+
+export default Sidebar
